4dca17211c78e1f099f22e198e811dada8e562d4
ivonatau <ivona.tautkute@gmail.com>
2017-04-13 21:16:34 +0300

    Update detector.c

--- src/detector.c
+++ src/detector.c
@@ -439,57 +439,6 @@ void validate_detector_recall(char *cfgfile, char *weightfile)
     }
 }
 
-// box * get_boxes_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh)
-// {
-//     FILE * fp;
-//     fp = fopen ("bounding_boxes.txt", "w+");
-//     printf("we are in the detector!\n");
-//     list *options = read_data_cfg(datacfg);
-//     char *name_list = option_find_str(options, "names", "data/names.list");
-//     char **names = get_labels(name_list);
-
-//     image **alphabet = load_alphabet();
-//     network net = parse_network_cfg(cfgfile);
-//     if(weightfile){
-//         load_weights(&net, weightfile);
-//     }
-//     set_batch_network(&net, 1);
-//     srand(2222222);
-//     clock_t time;
-//     char buff[256];
-//     char *input = buff;
-//     int j;
-//     float nms=.4;
-//     strncpy(input, filename, 256);
-//     // box *new_boxes;
-//     image im = load_image_color(input,0,0);
-//     image sized = resize_image(im, net.w, net.h);
-//     printf("Resizing to %i and %i", net.w, net.h);
-//     layer l = net.layers[net.n-1];
-//     box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
-//     float **probs = calloc(l.w*l.h*l.n, sizeof(float *));
-//     for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = calloc(l.classes + 1, sizeof(float *));
-//     float *X = sized.data;
-//     time=clock();
-//     network_predict(net, X);
-//     printf("%s: Predicted in %f seconds by detector.c.\n", input, sec(clock()-time));
-//     get_region_boxes(l, 1, 1, thresh, probs, boxes, 0, 0, hier_thresh);
-//     if (l.softmax_tree && nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-//     else if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-//     printf("%i boxes length\n", l.w*l.h*l.n );
-//     draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, names, alphabet, l.classes);
-//     save_image(im, filename);
-//     show_image(im, "predictions");
-
-//     free_image(im);
-//     free_image(sized);
-//     free_ptrs((void **)probs, l.w*l.h*l.n);
-
-//     fclose(fp);
-
-
-//     return boxes;
-// }
 
 void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh)
 {
