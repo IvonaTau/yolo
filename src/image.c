7cdf0dd04c531c84ea5175f96abd8669dd50fe56
ivonatau <ivona.tautkute@gmail.com>
2017-04-15 13:27:19 +0300

    Update image.c

--- src/image.c
+++ src/image.c
@@ -185,9 +185,12 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
     fp = fopen ("bounding_boxes.txt", "w+");
 
     fprintf(fp, "Dimensions;%i;%i\n", im.w, im.h);
+    fprintf(fp, "Thres;%.0f%%;\n", thresh); //remove this
+    fprintf(fp, "Num;%i;\n", num); //remove this
 
     for(i = 0; i < num; ++i){
         int class = max_index(probs[i], classes);
+        fprintf(fp, "Int class;%i;\n", class); //remove this
         float prob = probs[i][class];
         if(prob > thresh){
 
